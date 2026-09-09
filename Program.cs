c134860a6e80b7d83d8b2b03557905c7c606e8cf
valuskaoliver <valuska.oliver.2023d@telekimezotur.hu>
2026-09-09 10:31:55 +0200

    Első feladat

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Homerseklet
+{
+    static void Main(string[] args)
+    { 
+
+        List<int> homer = File.ReadAllLines("homersekletek.txt").Select(int.Parse).ToList();
+        
+        Console.WriteLine($"A beolvasott napok száma: {homer.Count()}");
+    }
+}
